700168498730e281e2a36088ef5a36e12f9b3748
viglinat-at-fedora <prokhorov1099@gmail.com>
2024-08-17 14:00:57 +0300

    Added solutions to ex1.8 and ex1.9. Solution to ex1.7 was corrected

--- ch1/solutions/ex9/main.go
+++ ch1/solutions/ex9/main.go
@@ -11,10 +11,14 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
@@ -26,5 +30,6 @@ func main() {
 			os.Exit(1)
 		}
 		resp.Body.Close()
+		fmt.Printf("Status code: %s (%d)\n", resp.Status, resp.StatusCode)
 	}
 }
