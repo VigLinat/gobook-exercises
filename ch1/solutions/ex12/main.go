4b35e6bad7d5499a8865978a9e0ce67d48c40482
viglinat-at-fedora <prokhorov1099@gmail.com>
2024-08-18 22:57:08 +0300

    Worked through exercise 1.10 and 1.11. Added solution to exercise 1.12

---
+++ ch1/solutions/ex12/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+)
+
+func main() {
+	if len(os.Args) > 1 && os.Args[1] == "web" {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			var err error
+			if err = r.ParseForm(); err != nil {
+				fmt.Fprintf(os.Stderr, "Error while parsing form: %v", err)
+				lissajous(w)
+				return
+			}
+			params := make(map[string]string)
+			for k, v := range r.Form {
+				params[k] = v[0]
+			}
+			lissajous2(w, params)
+		})
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+		return
+	}
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*float64(size)+0.5), size+int(y*float64(size)+0.5),
+				blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
+
+func lissajous2(out io.Writer, params map[string]string) {
+	cycles := 5
+	res := 0.001
+	size := 100
+	nframes := 64
+	delay := 8
+
+	for k, v := range params {
+		switch k {
+		case "cycles":
+			if ivalue, err := strconv.Atoi(v); err == nil {
+				cycles = ivalue
+			}
+		case "res":
+			if fvalue, err := strconv.ParseFloat(v, 64); err == nil {
+				res = fvalue
+			}
+		case "size":
+			if ivalue, err := strconv.Atoi(v); err == nil {
+				size = ivalue
+			}
+		case "nframes":
+			if ivalue, err := strconv.Atoi(v); err == nil {
+				nframes = ivalue
+			}
+		case "delay":
+			if ivalue, err := strconv.Atoi(v); err == nil {
+				delay = ivalue
+			}
+		}
+	}
+
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*float64(size)+0.5), size+int(y*float64(size)+0.5),
+				blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
+
+//!-main
